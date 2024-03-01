5f746eb0e00641d3ef7d074cedbcee446a966c36
Julia Costa <juliacosta232005@gmail.com>
2024-03-01 00:28:04 +0000

    Added Ask for Raio

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -9,6 +9,10 @@ namespace Cilindro
             Console.Write("Insira a altura do cilindro: ");
             string alt = Console.ReadLine();
             int a = int.Parse(alt);
+
+            Console.Write("Insira o raio da abse do cilindro: ");
+            string raio = Console.ReadLine();
+            int r = int.Parse(raio);
         }
     }
 }
