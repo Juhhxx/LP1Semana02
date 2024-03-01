82e23fcf73a5d5788a3315b791fc0d3ac981391a
Julia Costa <juliacosta232005@gmail.com>
2024-03-01 00:11:24 +0000

    Added SmoothOperator exercise

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Insira um número inteiro não negativo: ");
+            string num = Console.ReadLine();
+
+            byte n = byte.Parse(num);
+
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+    
+        }
+    }
+}
