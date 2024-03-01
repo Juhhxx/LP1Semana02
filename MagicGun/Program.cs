030bdccdcd5540c67965c0a51bcafbbdaddecb79
Julia Costa <juliacosta232005@gmail.com>
2024-03-01 16:42:11 +0000

    Added Eletric fire and string concatenation

--- MagicGun/Program.cs
+++ MagicGun/Program.cs
@@ -10,15 +10,19 @@ namespace MagicGun
             string num = Console.ReadLine();
             int n = int.Parse(num);
 
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i <= n; i++)
             {
                 if (i % 3 == 0)
                 {
-                    Console.WriteLine("Magic fire!");
+                    Console.WriteLine($"{i} : Magic fire!");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine($"{i} : Eletric Fire!");
                 }
                 else
                 {
-                    Console.WriteLine("Normal Fire :/");
+                    Console.WriteLine($"{i} : Normal Fire :/");
                 }
             }
         }
