6fd1488b25d401fd2ce56695344efee454becf6f
Julia Costa <juliacosta232005@gmail.com>
2024-03-01 16:45:16 +0000

    Changed string print format to acomodate future uses

--- MagicGun/Program.cs
+++ MagicGun/Program.cs
@@ -9,21 +9,24 @@ namespace MagicGun
             Console.Write("Insira um número inteiro: ");
             string num = Console.ReadLine();
             int n = int.Parse(num);
+            string shot;
 
             for (int i = 1; i <= n; i++)
             {
                 if (i % 3 == 0)
                 {
-                    Console.WriteLine($"{i} : Magic fire!");
+                    shot = "Magic fire!";
                 }
                 else if (i % 5 == 0)
                 {
-                    Console.WriteLine($"{i} : Eletric Fire!");
+                    shot = "Eletric Fire!";
                 }
                 else
                 {
-                    Console.WriteLine($"{i} : Normal Fire :/");
+                    shot = "Normal Fire :/";
                 }
+
+                Console.WriteLine($"{i} : {shot}");
             }
         }
     }
